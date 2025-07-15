f61b0f4c9f46f37a93539f99e5341f62481c9b0d
Timmattack <166731667+Timmattack@users.noreply.github.com>
2025-07-16 00:07:55 +0200

    premiers tests

--- Problemes/0108. Convert Sorted Array to Binary Search Tree/main.cpp
+++ Problemes/0108. Convert Sorted Array to Binary Search Tree/main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<vector>
+
+using namespace std;
 
 struct TreeNode {
     int val;
@@ -9,11 +12,61 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+void printBT(const std::string& prefix, const TreeNode* node, bool isLeft){
+    if( node != nullptr )
+    {
+        std::cout << prefix;
+
+        std::cout << (isLeft ? "├──" : "└──" );
+
+        // print the value of the node
+        std::cout << node->val << std::endl;
+
+        // enter the next tree level - left and right branch
+        printBT( prefix + (isLeft ? "│   " : "    "), node->left, true);
+        printBT( prefix + (isLeft ? "│   " : "    "), node->right, false);
+    }
+}
+
+void printBT(const TreeNode* node){
+    printBT("", node, false);
+}
 
 
+TreeNode* sortedArrayToBST(vector<int>& nums, int left, int right){
+    if(left < right){
+        int middle = (left+right)/2;
+        return new TreeNode(nums[middle],
+                            sortedArrayToBST(nums, left, middle-1),
+                            sortedArrayToBST(nums, middle+1, right)
+                            );
+    }
+    else return nullptr;
+}
 
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    if(nums.size() == 1) return new TreeNode(nums[0]);
+    if(nums.size() == 2) return new TreeNode(nums[0],
+                                               new TreeNode(nums[1]),
+                                               nullptr);
+    return sortedArrayToBST(nums, 0, nums.size()-1);
+}
 
 int main(){
+    vector<int> ex1 = {-1, -3, 0, 5, 9};
+    vector<int> ex2 = {1, 3};
+
+    TreeNode* t1 = sortedArrayToBST(ex1);
+    std::cout << "-----" << std::endl;
+    TreeNode* t2 = sortedArrayToBST(ex2);
+    std::cout << "-----" << std::endl;
+
+    std::cout << "t1 : " << std::endl;
+    printBT(t1);
+
+    std::cout << std::endl;
+    std::cout << "t2 : " << std::endl;
+    printBT(t2);
 
 
     return 0;
