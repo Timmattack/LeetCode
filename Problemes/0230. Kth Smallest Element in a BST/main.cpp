55e6df1f5de9d1a7b96967bf2f78263dbec72b6d
Timmattack <166731667+Timmattack@users.noreply.github.com>
2025-07-17 00:18:41 +0200

    Exemples ajouté

--- Problemes/0230. Kth Smallest Element in a BST/main.cpp
+++ Problemes/0230. Kth Smallest Element in a BST/main.cpp
@@ -10,9 +10,50 @@ TreeNode() : val(0), left(nullptr), right(nullptr) {}
 TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+void deleteTree(TreeNode* &root){
+    if(root){
+        deleteTree(root->left);
+        deleteTree(root->right);
+
+        delete root;
+    }
+}
+
+/*** ça commence ici ***/
+
+
+int kthSmallest(TreeNode* root, int k) {
+
+}
 
 
 int main(){
+    TreeNode* ex1 = new TreeNode(3,
+                        new TreeNode(1,
+                            nullptr,
+                            new TreeNode(2))
+                        new TreeNode(4)
+                    );
+
+    TreeNode* ex2 = new TreeNode(5,
+                        new TreeNode(3,
+                            new TreeNode(2,
+                                new TreeNode(1),
+                                nullptr
+                                ),
+                            new TreeNode(4)
+                        ),
+                        new TreeNode(6)
+                    );
+
+    int res1 = kthSmallest(ex1, 1);
+    int res2 = kthSmallest(ex2, 3);
+
+    cout << "ex1 : " << res1 << endl;
+    cout << "ex2 : " << res2 << endl;
+
+    deleteTree(ex1);
+    deleteTree(ex2);
 
     return 0;
 }
