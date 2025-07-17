ac34f26e41f995e5f77c6a89133a3e4d63ed558a
Timmattack <166731667+Timmattack@users.noreply.github.com>
2025-07-18 00:54:07 +0200

    1828 validé

--- Problemes/1828. Queries on number of Points Inside a Circle/main.cpp
+++ Problemes/1828. Queries on number of Points Inside a Circle/main.cpp
@@ -16,10 +16,26 @@ string vectToString(vector<int>& v){
 
     return msg;
 }
+int carre(int a){return a*a;}
 
+bool isInCircle(Point p, Cercle c){
+    return carre(p[0]-c[0]) + carre(p[1]-c[1]) <= c[2]*c[2];
+}
 
 vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+    vector<int> res;
 
+    int pointsInC = 0;
+    for(Cercle c : queries){
+        pointsInC = 0;
+
+        for(Point p : points){
+            pointsInC += isInCircle(p, c);
+        }
+        res.push_back(pointsInC);
+    }
+
+    return res;
 }
 
 
@@ -30,9 +46,11 @@ int main(){
     vector<Point> p2s = {{1,1},{2,2},{3,3},{4,4},{5,5}};
     vector<Cercle> c2s = {{1,2,2},{2,2,2},{4,3,2},{4,3,3}};
 
+    vector<int> res1 = countPoints(p1s, c1s);
+    vector<int> res2 = countPoints(p2s, c2s);
 
-    cout << "ex1 : " << vectToString(countPoints(p1s, c1s)) << endl;
-    cout << "ex2 : " << vectToString(countPoints(p2s, c2s)) << endl;
+    cout << "ex1 : " << vectToString(res1) << endl;
+    cout << "ex2 : " << vectToString(res2) << endl;
 
 
     return 0;
