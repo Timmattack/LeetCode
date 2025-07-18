153741059ff7313ffb957e407a8f97a302ff2445
Timmattack <166731667+Timmattack@users.noreply.github.com>
2025-07-19 01:23:43 +0200

    nexVoidInBoard corrigé pour de bon :)

--- Problemes/0037. Sudoku Solver/main.cpp
+++ Problemes/0037. Sudoku Solver/main.cpp
@@ -27,23 +27,21 @@ void printBoard(Board& B){
 // give your variables which represent the next i/j
 // -1/-1 if end of board
 void nextVoidInBoard(Board& board, int iStart, int jStart, int &iNext, int &jNext){
-    if(iStart == 8){
-        for(int j=jStart; j<9; j++){
-            if(board[8][j] == '.'){
-                iNext = 8;
-                jNext = j;
-                return ;
-            }
+
+    // search on the same line
+    for(int j=jStart+1; j<9; j++){
+        if(board[iStart][j] == '.'){
+            iNext = iStart;
+            jNext = j;
+            return ;
         }
-        iNext = -1;
-        jNext = -1;
-        return ;
     }
 
-    for(int i=iStart; i<9 ;i++){
-        for(int j=jStart+1; j<9; j++){
+    //search on next lines
+    for(int i=iStart+1; i<9 ;i++){
+        for(int j=0; j<9; j++){
             if(board[i][j] == '.'){
-                iNext = i;
+                iNext = iStart;
                 jNext = j;
                 return ;
             }
@@ -174,7 +172,7 @@ void _testPossibilitiesHere(Board &board, int line, int col){
 int main(){
     Board ex1 = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
 
-    _testNextVoidInBoard(ex1);
+    //_testNextVoidInBoard(ex1);
 
     //_testPossibilitiesHere(ex1, 0,2);
 
