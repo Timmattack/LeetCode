bbd507801ef11b1370e565566acd67462998cf13
Timmattack <166731667+Timmattack@users.noreply.github.com>
2025-07-18 18:14:05 +0200

    commit init + test 0374

---
+++ Problemes/0374. Guess Number Higher or Lower/main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+const int GO_LOWER = -1;
+const int GO_UPPER = 1;
+
+int guess(int num){return 0;}
+
+int guessNumber(int lower, int upper){
+    if(lower==upper) return lower;
+
+    int middle = (lower+upper)/2;
+
+    int goGuess = guess(middle);
+
+    switch(goGuess){
+        case GO_LOWER:
+            return guessNumber(lower, middle-1);
+
+        case GO_UPPER:
+            return guessNumber(middle+1, upper);
+
+        default:
+            return middle;
+    }
+}
+
+int guessNumber(int n) {
+    return guessNumber(1, n);
+}
+
+
+int main(){
+
+
+    return 0;
+}
