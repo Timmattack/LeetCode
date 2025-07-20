6217fdf26cf1db12ec123df0ac2d44039cd029aa
Timmattack <166731667+Timmattack@users.noreply.github.com>
2025-07-20 17:48:39 +0200

    à continuer

--- Problemes/0968. Binary Tree Cameras/main.cpp
+++ Problemes/0968. Binary Tree Cameras/main.cpp
@@ -12,9 +12,22 @@ TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
+bool isLeaf(TreeNode* root){
+    if(!root || root->left || root->right) return false;
+    else return true;
+}
 
-int minCameraCover(TreeNode* root, bool topHasCamera){
+/*
+ Idea :
+ If the parent has a camera : just go next, but tell them they are not covered.
+ Otherwise : check if left Child can cover you (check if it exists), otherwise check right child (check if it exists), otherwise cover yourself (you exist :D)
+ */
+int minCameraCover(TreeNode* root, bool parentHasCamera){
+    if(!root) return 0;
 
+    if(isLeaf(root)) return !parentHasCamera;
+
+    if()
 }
 
 int minCameraCover(TreeNode* root) {
